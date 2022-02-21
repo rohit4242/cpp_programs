73928d91bc626f7eb3405fd840e7b5a5480e201e
rohit4242 <rohitluni4242@gmail.com>
2022-02-21 19:35:14 +0530

    init

--- main.cpp
+++ main.cpp
@@ -3,6 +3,31 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    // find the prime number of the given numbers of the range
+    int n, i, j, k, l, m, count = 0;
+    cout << "Enter the range of the prime numbers: ";
+    cin >> n;
+    int prime[n];
+    for (i = 2; i <= n; i++)
+    {
+        for (j = 2; j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                break;
+            }
+        }
+        if (i == j)
+        {
+            prime[count] = i;
+            count++;
+        }
+    }
+    cout << "The prime numbers are: ";
+    for (k = 0; k < count; k++)
+    {
+        cout << prime[k] << " ";
+    }
+    cout << endl;
     return 0;
 }
