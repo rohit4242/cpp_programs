5d2ae68cc6940b3e2543df0118d44994c90108ef
rohit4242 <rohitluni4242@gmail.com>
2022-04-02 12:22:50 +0530

    init

--- Unit-2/program_8.cpp
+++ Unit-2/program_8.cpp
@@ -7,110 +7,49 @@
 using namespace std;
 
 
-class shape
+inline float area_circle(float radius)
 {
-    public:
-        virtual void volume() = 0;
-};
+	return 3.14*radius*radius;
+}
 
-class cube : public shape
+float volume(float cube)
 {
-    private:
-        int length;
-    public:
-        void volume()
-        {
-            cout << "Enter length: ";
-            cin >> length;
-            cout << "Volume of cube is: " << length*length*length << endl;
-        }
-};
+    return cube*cube*cube;
+}
 
-class cylinder : public shape
+// a function volume to calculate volume of a cylinder
+float volume(float cylinder, float radius)
 {
-    private:
-        int radius;
-        int height;
-    public:
-        void volume()
-        {
-            cout << "Enter radius: ";
-            cin >> radius;
-            cout << "Enter height: ";
-            cin >> height;
-            cout << "Volume of cylinder is: " << 3.14*radius*radius*height << endl;
-        }
-};
+    return 3.14*radius*radius*cylinder;
+}
 
-class box : public shape
+// a function volume to calculate volume of a rectangular box
+float volume(float length, float width, float height)
 {
-    private:
-        int length;
-        int breadth;
-        int height;
-    public:
-        void volume()
-        {
-            cout << "Enter length: ";
-            cin >> length;
-            cout << "Enter breadth: ";
-            cin >> breadth;
-            cout << "Enter height: ";
-            cin >> height;
-            cout << "Volume of box is: " << length*breadth*height << endl;
-        }
-};
+    return length*width*height;
+}
 
-class sphere : public shape
+// a function volume to calculate volume of a sphere
+float volume(float sphere, int radius)
 {
-    private:
-        int radius;
-    public:
-        void volume()
-        {
-            cout << "Enter radius: ";
-            cin >> radius;
-            cout << "Volume of sphere is: " << 3.14*radius*radius*radius << endl;
-        }
-};
-
-inline float area(int radius)
-{
-    return (3.14*radius*radius);
+    return 4/3*3.14*radius*radius*radius;
 }
 
 int main()
 {
-    shape *s;
-    int choice;
-    cout << "Enter choice: " << endl;
-    cout << "1. Cube" << endl;
-    cout << "2. Cylinder" << endl;
-    cout << "3. Box" << endl;
-    cout << "4. Sphere" << endl;
-    cin >> choice;
-    switch(choice)
-    {
-        case 1:
-            s = new cube;
-            s->volume();
-            break;
-        case 2:
-            s = new cylinder;
-            s->volume();
-            break;
-        case 3:
-            s = new box;
-            s->volume();
-            break;
-        case 4:
-            s = new sphere;
-            s->volume();
-            break;
-        default:
-            cout << "Invalid choice" << endl;
-    }
-    cout << "Area of circle is: " << area(5) << endl;
+    float cube = 5;
+    float cylinder = 5;
+    float radius = 5;
+    float length = 5;
+    float width = 5;
+    float height = 5;
+    float sphere = 5;
+
+    cout << "The volume of a cube is: " << volume(cube) << endl;
+    cout << "The volume of a cylinder is: " << volume(cylinder, radius) << endl;
+    cout << "The volume of a rectangular box is: " << volume(length, width, height) << endl;
+    cout << "The volume of a sphere is: " << volume(sphere, radius) << endl;
+    cout << "The area of a circle is: " << area_circle(radius) << endl;
+    
     return 0;
 }
-
