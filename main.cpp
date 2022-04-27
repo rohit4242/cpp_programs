f597b641f180316c071e2a67a1463a6d38f3ad4f
ROHIT LUNI <rohitluni123@gmail.com>
2022-04-27 10:39:58 +0530

    init

--- main.cpp
+++ main.cpp
@@ -1,33 +1,93 @@
 #include <iostream>
 using namespace std;
 
+class time 
+{
+    int hour;
+    int minute;
+    int second;
+
+    public:
+    time ()
+    {
+        hour = 0;
+        minute = 0;
+        second = 0;
+    }
+
+    time(int h, int m, int s)
+    {
+        hour = h;
+        minute = m;
+        second = s;
+    }
+
+    void get_time()
+    {
+        cout << "Enter the hour: "; 
+        cin >> hour;
+        cout << "Enter the minute: ";
+        cin >> minute;
+        cout << "Enter the second: ";
+        cin >> second;
+    }
+
+     void display_time()
+    {
+        cout << "Your Time is = " <<hour<< ":" <<minute << ":" <<second<<endl;
+    }
+
+    void add_time(time t1, time t2)
+    {
+        hour = t1.hour + t2.hour;
+        minute = t1.minute + t2.minute;
+        second = t1.second + t2.second;
+
+        if(second >= 60)
+        {
+            minute++;
+            second = second - 60;
+        }
+
+        if(minute >= 60)
+        {
+            hour++;
+            minute = minute - 60;
+        }
+
+        if(hour >= 24)
+        {
+            hour = hour - 24;
+        }
+    }
+
+    time operator +(time t2)
+    {
+        time ans;
+        ans.hour = hour + t2.hour;
+        ans.minute = minute + t2.minute;
+        ans.second = second + t2.second;
+
+        return ans;
+    }
+
+};
+
 int main()
 {
-    // find the prime number of the given numbers of the range
-    int n, i, j, k, count = 0;
-    cout << "Enter the range of the prime numbers: ";
-    cin >> n;
-    int prime[n];
-    for (i = 2; i <= n; i++)
-    {
-        for (j = 2; j <= i; j++)
-        {
-            if (i % j == 0)
-            {
-                break;
-            }
-        }
-        if (i == j)
-        {
-            prime[count] = i;
-            count++;
-        }
-    }
-    cout << "The prime numbers are: ";
-    for (k = 0; k < count; k++)
-    {
-        cout << prime[k] << " ";
-    }
-    cout << endl;
+    time t1(1,2,3), t2, t3, t4;
+
+    // t1.get_time();
+    t2.get_time();
+
+    t1.display_time();
+    // t2.display_time();
+
+    t3.add_time(t1,t2);
+    t3.display_time();
+
+    t4 = t1 + t2;
+    t4.display_time();
+
     return 0;
 }
