a33a98c47eb477a488d6780d349ec50e16a86130
ROHIT LUNI <rohitluni123@gmail.com>
2022-05-04 09:45:52 +0530

    init

--- Unit-4/program_2.cpp
+++ Unit-4/program_2.cpp
@@ -7,51 +7,78 @@ using namespace std;
 
 class student
 {
-    int rollno;
-    string name;
-    int m1, m2, m3;
-    float per;
-    char result;
+    private:
+        int rollno;
+        string name;
+        int m1;
+        int m2;
+        int m3;
+        float per;
+        string result;
+        char grade;
     public:
-    void getdata()
-    {
-        cout << "Enter rollno: ";
-        cin >> rollno;
-        cout << "Enter name: ";
-        cin >> name;
-        cout << "Enter marks of 3 subjects: ";
-        cin >> m1 >> m2 >> m3;
-        per = (m1 + m2 + m3)/3;
-        if(per >= 90)
-            result = 'A';
-        else if(per >= 80)
-            result = 'B';
-        else if(per >= 70)
-            result = 'C';
-        else if(per >= 60)
-            result = 'D';
-        else
-            result = 'F';
-    }
-    void dispdata()
-    {
-        cout << "Rollno: " << rollno << endl;
-        cout << "Name: " << name << endl;
-        cout << "Marks of 3 subjects: " << m1 << " " << m2 << " " << m3 << endl;
-        cout << "Percentage: " << per << endl;
-        cout << "Result: " << result << endl;
-    }
-    void cal()
-    {
-        if(per >= 90)
-            result = 'A';
-        else if(per >= 80)
-            result = 'B';
-        else if(per >= 70)
-            result = 'C';
-        else if(per >= 60)
-            result = 'D';
-        else
-            result = 'F';
-    }
+        void getdata()
+        {
+            cout << "Enter rollno: ";
+            cin >> rollno;
+            cout << "Enter name: ";
+            cin >> name;
+            cout << "Enter m1: ";
+            cin >> m1;
+            cout << "Enter m2: ";
+            cin >> m2;
+            cout << "Enter m3: ";
+            cin >> m3;
+            cal();
+        }
+        void dispdata()
+        {
+            cout << "================="<< endl;
+            cout << "Rollno: " << rollno << endl;
+            cout << "Name: " << name << endl;
+            cout << "M1: " << m1 << endl;
+            cout << "M2: " << m2 << endl;
+            cout << "M3: " << m3 << endl;
+            cout << "Per: " << per << endl;
+            cout << "Grade: " << grade << endl;
+            cout << "Result: " << result << endl;
+        }
+        // cal function is inline function
+        inline void cal()
+        {
+            per = ((m1+m2+m3)/3);
+            if(per >= 80)
+            {
+                result = "Pass";
+            }
+            else
+            {
+                result = "Fail";
+            }
+            if(per >= 80)
+            {
+                grade = 'A';
+            }
+            else if(per >= 60)
+            {
+                grade = 'B';
+            }
+            else if(per >= 40)
+            {
+                grade = 'C';
+            }
+            else
+            {
+                grade = 'D';
+            }
+
+        }
 };
+
+int main()
+{
+    student s;
+    s.getdata();
+    s.dispdata();
+    return 0;
+}
--- Unit-4/program_6.cpp
+++ Unit-4/program_6.cpp
@@ -11,7 +11,6 @@ class Counter
 {
     public:
         int count;
-
         Counter()
         {
             count = 0;
