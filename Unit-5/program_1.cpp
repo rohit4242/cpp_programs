9782bb7c5bcd5cc36bb45991431b51c6c1e99018
ROHIT LUNI <rohitluni123@gmail.com>
2022-05-13 10:55:21 +0530

    init

--- Unit-5/program_1.cpp
+++ Unit-5/program_1.cpp
@@ -1,86 +1,96 @@
+class student {
 
-#include <iostream>
-#include <fstream>
-using namespace std;
+    public:
+        char name[32]; 
+        char id[15];
+        int results;
+        string grade;
 
-class Student 
-{
-    private:
-        int id;
-        string name;
-        int age;
+        void add_record();
+        void display_record();
+        void search_by_name();
+        void search_by_id();
+        void print_grade(int result);
+    };
 
-        public:
+    void student::search_by_name(){
+        char sname[32];
+        student obj;
+        ifstream file ("Text_File.txt");
 
-        void getdata()
-        {
-            cout << "Enter the id: ";
-            cin >> id;
-            cout << "Enter the name: ";
-            cin >> name;
-            cout << "Enter the age: ";
-            cin >> age;
-        }
+            cout << "Enter name to find: ";
+            cin >> sname;
 
-        void putdata()
-        {        
-            cout << "The id is: " << id << endl;
-            cout << "The name is: " << name << endl;
-            cout << "The age is: " << age << endl;
-        }
-};
+        if (file.is_open()) {
 
-int main()
-{
-    Student s;
-    fstream file;
-    int choice;
+            if (!file.eof()) {
 
-    file.open("student.dat", ios::app | ios::in | ios::out | ios::binary);
-
-    do
-    {
-        cout << "1. Enter the data" << endl;
-        cout << "2. Display the data" << endl;
-        cout << "3. Check the current positions of record" << endl;
-        cout << "4. find the data" << endl;
-        cout << "5. Exit" << endl;
-
-        cout << "Enter your choice: ";
-        cin >> choice;
-
-        switch(choice)
-        {
-            case 1:
-                cout << "Enter the data" << endl;
-                s.getdata();
-                file.write((char*)&s, sizeof(s));
-                break;
-            case 2:
-                cout << "Display the data" << endl;
-                file.seekg(0, ios::beg);
-                while(file.read((char*)&s, sizeof(s)))
-                {
-                    s.putdata();
+                if(name == sname) {
+                    file.read((char*)& sname,sizeof(sname));
+                    cout << "\n Student Name:\t" << name;
+                    cout << "\n Student ID:\t" << id;
+                    cout << "\n Results:\t" << results;
+                    cout << "\n Grade:\t" ;
+                    obj.print_grade(results);}
                 }
-                break;
-            case 3:
-                int position = file.tellg();
-                cout << "The current position is: " << position << endl;
-                break;
-            case 4:
-                int position = file.tellg();
-                cout << "The current position is: " << position << endl;
-                int n = (position / sizeof(s));
 
-                cout << "The current record is: " << n << endl;
-                break;
-
-            default:
-                cout << "Invalid choice" << endl;
-
-        }
-    }while(choice != 3);
-
-    file.close();
+                else {
+                    cout << "Student not found.";
+          } 
+          else {
+                cout << "Unable to open file.";
+            } 
+            }
 }
+
+    void student::search_by_id(){
+        char id[15];
+        int result;
+        student obj;
+        ifstream file ("Text_File.txt");
+
+        cout << "Enter ID number: ";
+        cin >> id;
+
+        if (file.is_open())
+            if (file >> id) {
+                cout << "\n Student Name:\t" << obj.name;
+                cout << "\n Student ID:\t" << obj.id;
+                cout << "\n Results:\t" << obj.results;
+                cout << "\n Grade:\t" ;
+                obj.print_grade(obj.results);
+            }
+
+            else {
+                cout << "Name not found";
+            }
+        else {
+            cout << "Unable to open file.";
+        }
+    }
+
+    int main () {
+        student obj;
+        int choice;
+
+        cout << "\n Choose search method: ";
+        cout << "\n 1. Find by name.";
+        cout << "\n 2. Find by ID";
+        cout << "\n\n Enter your choice: ";
+        cin >> choice;    
+
+        switch (choice) {
+
+        case 1:
+            obj.search_by_name();
+            break;
+
+        case 2:
+            obj.search_by_id();
+            break;
+
+        default:
+            cout << "Invalid choice! Please enter 1 or 2 as your choice.";
+            break;
+        }           
+    }
