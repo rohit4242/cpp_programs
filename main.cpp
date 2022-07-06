c29ba36ef7e84ba14612b5ecd981c3003e39dea0
Rohit Luni <87267331+rohit4242@users.noreply.github.com>
2022-07-06 14:11:16 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,90 +1,94 @@
-// Help a Thief!!! 
-
-// You have to help a thief to steal as many as GoldCoins as possible from a GoldMine. There he saw N Gold Boxes an each Gold Boxes consists of Ai Plates each plates consists of Bi Gold Coins. Your task is to print the maximum gold coins theif can steal if he can take a maximum of T plates.
-
- 
-
-// Example 1:
-
-// Input:
-// T = 3, N = 3 
-// A[] = {1, 2, 3}
-// B[] = {3, 2, 1}
-// Output:
-// 7
-// Explanation:
-// The thief will take 1 plate of coins
-// from the first box and 2 plate of coins
-// from the second plate. 3 + 2*2 = 7.
-// Example 2:
-
-// Input:
-// T = 0, N = 3 
-// A[] = {1, 3, 2}
-// B[] = {2, 3, 1}
-// Output:
-// 0
-// Explanation:
-// The thief can't take any plates.
-// So he can't steal any coins.
- 
-
-// Your Task:
-// You don't need to read input or print anything. Your task is to complete the function maxCoins() which takes 2 Integers T, and N and two arrays A and B of size N as input and returns the maximum number of gold coins the thief can steal if he can take a maximum of T plates.
-
- 
-
-// Expected Time Complexity: O(N*logN)
-// Expected Auxiliary Space: O(N)
-
- 
-
-// Constraints:
-// 0 <= T,N <= 104
-// 1 <= A[i] <= 104
-// 1 <= B[i] <= 104
-
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-class Solution {
-  public:
-    int maxCoins(int A[], int B[], int T, int N) {
-        // code here
-        int coins = 0;
-        while (T--){
-            int maks = -1, maksindex = -1;
-            for (int i = 0; i < N; i++){
-                if (A[i] > 0 and maks < B[i]){
-                    maks = B[i];
-                    maksindex = i;
+class menu
+{
+    int num[30],n,i;
+
+    public:
+        void add(int n)
+        {
+            for(int i=0;i<n;i++)
+            {
+                cin>>num[i];
+            }
+        }
+
+        void update(int index,int new_value)
+        {
+            num[index-1]=new_value;
+        }
+
+        void remove(int index,int n)
+        {
+            int new_num[30];
+            for(int i=0;i<n;i++)
+            {
+                if(num[i]!=index)
+                {
+                    new_num[i]=num[i];
+                    cout<<"your value is removed: "<<new_num[i]<<endl;
                 }
             }
-            coins += B[maksindex];
-            A[maksindex]--;
-           
         }
-        return coins;
-    }
+
+        void display(int n)
+        {
+            for(int i=0;i<n;i++)
+            {
+                cout<<num[i]<<endl;
+            }
+        }
 
 };
 
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int T,N;
-        cin>>T>>N;
-        
-        int A[N], B[N];
-        
-        for(int i=0; i<N; i++)
-            cin>>A[i];
-        for(int i=0; i<N; i++)
-            cin>>B[i];
+int main()
+{
+    int index,n,new_value,ch,w;
+    menu m;
+    cout<<"enter the 0 to exit the program: "<<endl;
+    cin>>w;
+    do{
+        cout<<"1.add value"<<endl;
+        cout<<"2.update value"<<endl;
+        cout<<"3.remove value"<<endl;
+        cout<<"4.display value"<<endl;
+        cout<<"5.exit"<<endl;
 
-        Solution ob;
-        cout << ob.maxCoins(A,B,T,N) << endl;
-    }
+        cout<<"Enter your choice: "<<endl;
+        cin>>ch;
+
+        switch(ch)
+        {
+            case 1:
+                cout<<"how many value you want to add: "<<endl;
+                cin>>n;
+                m.add(n);
+                cout<<"your value hase been added successfully"<<endl;
+                break;
+            case 2:
+                cout<<"Enter index number: "<<endl;
+                cin>>index;
+                cout<<"Enter new value: "<<endl;
+                cin>>new_value;
+                m.update(index,new_value);
+                cout<<"your value hase been update successfully"<<endl;
+                break;
+            case 3:
+                cout<<"Enter index number: "<<endl;
+                cin>>index;
+                m.remove(index,n);
+                cout<<"your value hase been update successfully"<<endl;
+                break;
+            case 4:
+                m.display(n);
+                break;
+            case 5:
+                exit(0);
+            default:
+                cout<<"Invalid operation"<<endl;
+        }
+    }while (w!=0);
+    
     return 0;
-} 
+}
